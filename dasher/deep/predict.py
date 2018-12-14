e1596dec9cf15537992bf771570b8465d1521feb
mgoubran <magedgoubran@gmail.com>
2018-12-14 15:04:59 -0500

    added deep module

---
+++ dasher/deep/predict.py
@@ -0,0 +1,101 @@
+import os
+import nibabel as nib
+import numpy as np
+
+from keras.models import load_model, model_from_json
+from keras_contrib.layers import InstanceNormalization
+from hypermatter.deep.metrics import (dice_coefficient, dice_coefficient_loss, dice_coef, dice_coef_loss,
+                                      weighted_dice_coefficient_loss, weighted_dice_coefficient)
+import warnings
+warnings.simplefilter("ignore", RuntimeWarning)
+warnings.simplefilter("ignore", FutureWarning)
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = "3"
+
+
+def load_old_model_json(model_json):
+
+    print("\n loading pre-trained model")
+
+    custom_objects = {'dice_coefficient_loss': dice_coefficient_loss, 'dice_coefficient': dice_coefficient,
+                      'dice_coef': dice_coef, 'dice_coef_loss': dice_coef_loss,
+                      'weighted_dice_coefficient': weighted_dice_coefficient,
+                      'weighted_dice_coefficient_loss': weighted_dice_coefficient_loss}
+    try:
+        custom_objects["InstanceNormalization"] = InstanceNormalization
+    except ImportError:
+        pass
+    try:
+        return model_from_json(model_json, custom_objects=custom_objects)
+    except ValueError as error:
+        if 'InstanceNormalization' in str(error):
+            raise ValueError(str(error) + "\n\nPlease install keras-contrib to use InstanceNormalization:\n"
+                                          "'pip install git+https://www.github.com/keras-team/keras-contrib.git'")
+        else:
+            raise error
+
+
+def get_prediction_labels(prediction, threshold=0.5, labels=None):
+    n_samples = prediction.shape[0]
+    label_arrays = []
+
+    for sample_number in range(n_samples):
+        label_data = np.argmax(prediction[sample_number], axis=0) + 1
+        label_data[np.max(prediction[sample_number], axis=0) < threshold] = 0
+        if labels:
+            for value in np.unique(label_data).tolist()[1:]:
+                label_data[label_data == value] = labels[value - 1]
+        label_arrays.append(np.array(label_data, dtype=np.uint8))
+
+    return label_arrays
+
+
+def prediction_to_image(prediction, affine, label_map=False, threshold=0.5, labels=None):
+    if prediction.shape[1] == 1:
+        data = prediction[0, 0]
+        # if label_map:
+        #     label_map_data = np.zeros(prediction[0, 0].shape, np.int8)
+        #     if labels:
+        #         if isinstance(labels, int):
+        #             label = 1
+        #         else:
+        #             label = labels[0]
+        #     else:
+        #         label = 1
+        #     label_map_data[data > threshold] = label -- will threshold after resampling to original
+        #     data = label_map_data
+    elif prediction.shape[1] > 1:
+        if label_map:
+            label_map_data = get_prediction_labels(prediction, threshold=threshold, labels=labels)
+            data = label_map_data[0]
+        else:
+            return multi_class_prediction(prediction, affine)
+    else:
+        raise RuntimeError("Invalid prediction array shape: {0}".format(prediction.shape))
+
+    return nib.Nifti1Image(data, affine)
+
+
+def multi_class_prediction(prediction, affine):
+    prediction_images = []
+
+    for i in range(prediction.shape[1]):
+        prediction_images.append(nib.Nifti1Image(prediction[0, i], affine))
+
+    return prediction_images
+
+
+def run_test_case(test_data, model_json, model_weights, affine,
+                  output_label_map=False, threshold=0.5, labels=None):
+
+    json_file = open(model_json, 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    model = load_old_model_json(loaded_model_json)
+
+    model.load_weights(model_weights)
+
+    prediction = model.predict(test_data)
+
+    return prediction_to_image(prediction, affine, label_map=output_label_map, threshold=threshold,
+                               labels=labels)
---
+++ dasher/deep/save_weights.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# coding: utf-8
+
+from keras.models import load_model
+from keras_contrib.layers import InstanceNormalization
+import hypermatter.deep.metrics
+import sys
+import os
+
+in_model = sys.argv[1]
+model_name = sys.argv[2]
+
+custom_objects = {'dice_coefficient_loss': hypermatter.deep.metrics.dice_coefficient_loss,
+                  'dice_coefficient': hypermatter.deep.metrics.dice_coefficient,
+                  'dice_coef': hypermatter.deep.metrics.dice_coef,
+                  'dice_coef_loss': hypermatter.deep.metrics.dice_coef_loss,
+                  'weighted_dice_coefficient': hypermatter.deep.metrics.weighted_dice_coefficient,
+                  'weighted_dice_coefficient_loss': hypermatter.deep.metrics.weighted_dice_coefficient_loss,
+                  "InstanceNormalization": InstanceNormalization}
+
+model = load_model(in_model, custom_objects=custom_objects)
+model_json = model.to_json() 
+
+with open("%s.json" % model_name, "w") as json_file:
+    json_file.write(model_json)
+
+print("Saving model weights")
+
+model.save_weights('%s_weights.h5' % model_name)
+
+print("Model weights and json saved")
