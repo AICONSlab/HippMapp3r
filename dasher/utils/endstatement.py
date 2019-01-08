1da604850af1590b07ff22d44e91435ddb5d0a5b
Emmanuel Edward Ntiri <ntiri.e@gmail.com>
2019-01-08 14:14:23 -0500

    Committing file that produces last line of processing

--- dasher/utils/endstatement.py
+++ dasher/utils/endstatement.py
@@ -3,7 +3,7 @@
 # coding: utf-8
 
 import os
-# import pwd
+import pwd
 import re
 from datetime import datetime
 import warnings
@@ -15,24 +15,24 @@ def main(task=None, timediff=None):
     """
     Generates end statement based on function/task and time difference
     """
-    # name = pwd.getpwuid(os.getuid())[4]
-    #
-    # if re.search('[a-zA-Z]', name):
-    #     user = name.split(" ")[0]
-    #     user = user.replace(',', '').strip()
-    # else:
-    #     user = os.environ['USER']
-    #
-    # if 6 < datetime.now().hour < 12:
-    #     timeday = 'morning'
-    # elif 12 <= datetime.now().hour < 18:
-    #     timeday = 'afternoon'
-    # elif 18 <= datetime.now().hour < 22:
-    #     timeday = 'evening'
-    # else:
-    #     timeday = 'night'
-    #
-    # print("\n %s done in %s ... Have a good %s %s!\n" % (task, timediff, timeday, user))
+    name = pwd.getpwuid(os.getuid())[4]
+
+    if re.search('[a-zA-Z]', name):
+        user = name.split(" ")[0]
+        user = user.replace(',', '').strip()
+    else:
+        user = os.environ['USER']
+
+    if 6 < datetime.now().hour < 12:
+        timeday = 'morning'
+    elif 12 <= datetime.now().hour < 18:
+        timeday = 'afternoon'
+    elif 18 <= datetime.now().hour < 22:
+        timeday = 'evening'
+    else:
+        timeday = 'night'
+
+    print("\n %s done in %s ... Have a good %s %s!\n" % (task, timediff, timeday, user))
 
 
 if __name__ == "__main__":
