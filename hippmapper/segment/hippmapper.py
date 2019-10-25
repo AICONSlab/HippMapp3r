002eac249e57b57248a858167aecdcc6dbc14300
mgoubran <magedgoubran@gmail.com>
2019-10-25 17:01:53 -0400

    replace hypermatter

--- hippmapper/segment/hippmapper.py
+++ hippmapper/segment/hippmapper.py
@@ -13,10 +13,10 @@ import numpy as np
 import nibabel as nib
 import subprocess
 from nilearn.image import reorder_img, resample_img, resample_to_img, math_img, largest_connected_component_img
-from hypermatter.deep.predict import run_test_case
-from hypermatter.utils import endstatement
-from hypermatter.preprocess import biascorr, trim_like
-from hypermatter.qc import seg_qc
+from hippmapper.deep.predict import run_test_case
+from hippmapper.utils import endstatement
+from hippmapper.preprocess import biascorr, trim_like
+from hippmapper.qc import seg_qc
 from nipype.interfaces.fsl import maths
 from nipype.interfaces.c3 import C3d
 from termcolor import colored
@@ -30,11 +30,11 @@ def parsefn():
                                            "works best with a bias-corrected with-skull or skull-tripped image in"
                                            " standard orientation (RPI or LPI)\n\n"
                                            "Examples: \n"
-                                           "    hypermatter segment_hipp -t1 my_subj/mprage.nii.gz \n"
+                                           "    hippmapper segment_hipp -t1 my_subj/mprage.nii.gz \n"
                                            "OR (to bias-correct before and overwrite existing segmentation)\n"
-                                           "    hypermatter segment_hipp -t1 my_subj/mprage.nii.gz -b -f \n"
+                                           "    hippmapper segment_hipp -t1 my_subj/mprage.nii.gz -b -f \n"
                                            "OR (to run for subj - looks for my_subj_T1_nu.nii.gz)\n"
-                                           "    hypermatter segment_hipp -s my_subj \n")
+                                           "    hippmapper segment_hipp -s my_subj \n")
 
     optional = parser.add_argument_group('optional arguments')
 
