8f369daadccae102b18a8527379519a67ef4e7c1
Emmanuel Edward Ntiri <ntiri.e@gmail.com>
2019-12-06 16:00:33 -0500

    added line to set env variable

--- install_depends.sh
+++ install_depends.sh
@@ -10,10 +10,10 @@ mkdir -p "${SCRIPTPATH}/depends/"
 antspath=$( command -v antsRegistration )
 
 if [[ -z "${antspath}" ]]; then
-    export ANTSPATH="${SCRIPTPATH}/depends/ANTs"
+    export ANTSPATH="ANTSPATH:${SCRIPTPATH}/depends/ANTs"
     mkdir -p "${SCRIPTPATH}/depends/ANTs" && \
     curl -sSL "https://dl.dropbox.com/s/2f4sui1z6lcgyek/ANTs-Linux-centos5_x86_64-v2.2.0-0740f91.tar.gz" \
-    tar -xzC $ANTSPATH --strip-components 1
+    | tar -xzC $ANTSPATH --strip-components 1
 fi
 
 
@@ -24,5 +24,5 @@ if [[ -z "${c3dpath}" ]]; then
     wget https://downloads.sourceforge.net/project/c3d/c3d/Nightly/c3d-nightly-Linux-x86_64.tar.gz && \
     tar -xzvf c3d-nightly-Linux-x86_64.tar.gz && mv c3d-1.1.0-Linux-x86_64 "${SCRIPTPATH}/depends/c3d/" && \
     rm c3d-nightly-Linux-x86_64.tar.gz
-    export PATH="${SCRIPTPATH}/depends/c3d/:$PATH"
+    export PATH="${SCRIPTPATH}/depends/c3d/bin/:$PATH"
 fi
