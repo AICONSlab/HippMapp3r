9cd1de6d24187f8a94121b57f55f232b4129c63a
entiri <ntiri.e@gmail.com>
2020-02-10 16:59:28 -0500

    updating model to tf2

--- setup.py
+++ setup.py
@@ -40,8 +40,8 @@ setup(
         'keras-contrib', 'pandas', 'numpy', 'plotly', 'PyQt5'
     ],
     extras_require={
-        "hippmapper": ["tensorflow<=1.5.0"],
-        "hippmapper_gpu": ["tensorflow-gpu<=1.5.0"],
+        "hippmapper": ["tensorflow>=1.4.0"],
+        "hippmapper_gpu": ["tensorflow-gpu>=1.4.0"],
     },
     entry_points={'console_scripts': ['hippmapper=hippmapper.cli:main']},
     keywords=[
