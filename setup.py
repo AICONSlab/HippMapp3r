9a1a6d9fe950c431fe0ee64fb9f5f0fa2e2fc970
Emmanuel Edward Ntiri <ntiri.e@gmail.com>
2020-04-07 16:25:44 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
         'git+https://github.com/keras-team/keras-contrib.git'
     ],
     install_requires=[
-        'nibabel', 'nipype', 'argparse', 'argcomplete', 'joblib', 'keras==2.2.2', 'nilearn', 'scikit-learn',
+        'nibabel', 'nipype', 'argparse', 'argcomplete', 'joblib', 'keras==2.2.0', 'nilearn', 'scikit-learn',
         'keras-contrib', 'pandas', 'numpy', 'plotly', 'PyQt5'
     ],
     extras_require={
