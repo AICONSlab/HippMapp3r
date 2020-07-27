66fd082e86743f7b86acadab37810519036bfb21
entiri <ntiri.e@gmail.com>
2020-07-27 01:07:53 -0400

    updating setup.py

--- setup.py
+++ setup.py
@@ -36,12 +36,12 @@ setup(
         'git+https://github.com/keras-team/keras-contrib.git'
     ],
     install_requires=[
-        'nibabel', 'nipype', 'argparse', 'argcomplete', 'joblib', 'keras==2.2.4', 'nilearn', 'scikit-learn',
+        'nibabel', 'nipype', 'argparse', 'argcomplete', 'joblib', 'keras', 'nilearn', 'scikit-learn',
         'keras-contrib', 'pandas', 'numpy', 'plotly', 'PyQt5'
     ],
     extras_require={
-        "hippmapper": ["tensorflow==1.4.0"],
-        "hippmapper_gpu": ["tensorflow-gpu==1.4.0"],
+        "hippmapper": ["tensorflow"],
+        "hippmapper_gpu": ["tensorflow-gpu"],
     },
     entry_points={'console_scripts': ['hippmapper=hippmapper.cli:main']},
     keywords=[
